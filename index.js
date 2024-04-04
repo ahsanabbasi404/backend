a10e7873e420fc219039e46fea00a587789e0b8d
ahsanabbasi404 <abbasiahsan699@gmail.com>
2024-04-04 15:08:12 +0500

    added git file

--- index.js
+++ index.js
@@ -2,7 +2,40 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 // const port = 3000;
-
+const githubinfo = {
+    "login": "ahsanabbasi404",
+    "id": 155879791,
+    "node_id": "U_kgDOCUqJbw",
+    "avatar_url": "https://avatars.githubusercontent.com/u/155879791?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/ahsanabbasi404",
+    "html_url": "https://github.com/ahsanabbasi404",
+    "followers_url": "https://api.github.com/users/ahsanabbasi404/followers",
+    "following_url": "https://api.github.com/users/ahsanabbasi404/following{/other_user}",
+    "gists_url": "https://api.github.com/users/ahsanabbasi404/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/ahsanabbasi404/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/ahsanabbasi404/subscriptions",
+    "organizations_url": "https://api.github.com/users/ahsanabbasi404/orgs",
+    "repos_url": "https://api.github.com/users/ahsanabbasi404/repos",
+    "events_url": "https://api.github.com/users/ahsanabbasi404/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/ahsanabbasi404/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": null,
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 5,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2024-01-07T09:03:02Z",
+    "updated_at": "2024-04-04T09:45:39Z"
+  };
 app.get('/', (req, res) => {
   res.send('Hi!');
 })
@@ -11,6 +44,9 @@ app.get('/signout', (req, res) => {
 }) 
 app.get('/login', (req, res) => {
     res.send('Welcome to login page!');
+})
+app.get('/github', (req, res) => {
+    res.json(githubinfo);
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
